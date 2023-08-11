7ab6a6d5249e48c60dac12b771f7f4ce7fc8768f
CaioMorais15 <franciscocaio618@gmail.com>
2023-08-11 14:00:44 -0300

    finalizando links

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,10 +1,36 @@
-import "./App.css";
-import Profile from "..components/profile";
+import { useState } from "react";
+import Profile from "../components/profile/profile";
+import Switch from "../components/profile/switch/switch";
+import Links from "../components/links/links";
 
-export default function App(){
-    return(
-        <>
-       <Profile></Profile>
-    </>
-    );
-}
+import "./App.css";
+
+import imgDarkMode from "../img/avatar.jpg";
+import imgLightMode from "../img/avatar-light.jpg";
+
+
+const App = () => {
+  const [isLightMode, setIsLightMode] = useState(false);
+
+  const imgSrc = isLightMode ? imgLightMode : imgDarkMode;
+  const imgAlt = isLightMode ? "Imagem no Modo claro" : "Imagem no Modo escuro";
+
+  return (
+    <div id="container">
+      <Profile imgSrc={imgSrc} imgAlt={imgAlt}>
+        @ProfCastello
+      </Profile>
+      <Switch></Switch>
+      <div id="links">
+        <ul>
+          <Links link={"https://github.com"}>GitHub</Links>
+          <Links link={"#"}>Instagram</Links>
+          <Links link={"#"}>Portifolio</Links>
+          <Links link={"#"}>Projetos</Links>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default App;
